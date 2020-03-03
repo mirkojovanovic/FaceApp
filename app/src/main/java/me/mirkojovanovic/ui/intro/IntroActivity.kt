ef1ec899cdb9d799ea57196a675f945f178b99f3
mirkojovanovic <mirkoj1996@gmail.com>
2020-03-03 12:50:15 +0100

    layout for fragments

---
+++ app/src/main/java/me/mirkojovanovic/ui/intro/IntroActivity.kt
@@ -0,0 +1,13 @@
+package me.mirkojovanovic.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.mirkojovanovic.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
